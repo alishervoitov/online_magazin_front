f8fdbbd7009c1e8c9c5b2c5868daacfcbfee1ee7
Alisher Voitov <voitovalisher020@gmail.com>
2026-09-25 15:59:12 +0500

    himoyalash

--- src/App.jsx
+++ src/App.jsx
@@ -7,23 +7,41 @@ import Login from './components/Login';
 import Register from './components/Register';
 import Checkout from './components/Checkout';
 import Profile from './components/Profile';
+import ProtectedRoute from './components/ProtectedRoute'; // <-- Yangi import
 
 function App() {
     return (
         <div className="min-h-screen bg-gray-100">
             <Navbar />
             <Routes>
+                {/* Ommaviy sahifalar (hamma kira oladi) */}
                 <Route path="/" element={<Home />} />
                 <Route path="/products/:id" element={<ProductDetail />} />
                 <Route path="/cart" element={<Cart />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/register" element={<Register />} />
-                <Route path="/checkout" element={<Checkout />} />
-                <Route path="/profile" element={<Profile />} />
+
+                {/* Himoyalangan sahifalar (faqat tokeni borlar kira oladi) */}
+                <Route
+                    path="/checkout"
+                    element={
+                        <ProtectedRoute>
+                            <Checkout />
+                        </ProtectedRoute>
+                    }
+                />
+                <Route
+                    path="/profile"
+                    element={
+                        <ProtectedRoute>
+                            <Profile />
+                        </ProtectedRoute>
+                    }
+                />
             </Routes>
         </div>
     );
 }
 
-export default App;
+default export App;
 
